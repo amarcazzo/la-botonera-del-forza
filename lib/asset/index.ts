f61c5f8edc20d13fa54027754185a84853f9bb25
Ayrton Marcazzo <ayrtonmarcazzo@gmail.com>
2021-05-07 19:40:50 -0300

    fix(sa, google): change .json auth to private.key/email authentication

--- lib/asset/index.ts
+++ lib/asset/index.ts
@@ -1,15 +1,19 @@
-import { Storage } from "@google-cloud/storage";
-import config from "../config";
+import { Storage } from '@google-cloud/storage';
+import config from '../config';
 
 const storage = new Storage({
   projectId: config.BUCKET_PROJECT_ID,
+  credentials: {
+    client_email: config.BUCKET_EMAIL,
+    private_key: config.BUCKET_PRIVATE_KEY
+  }
 });
 
 const add = async (filePath: string, fileName: string) => {
   try {
     const bucket = await storage.bucket(config.BUCKET_NAME);
     const file = await bucket.upload(filePath, {
-      destination: fileName,
+      destination: fileName
     });
 
     return file;
--- lib/config.ts
+++ lib/config.ts
@@ -1,11 +1,11 @@
 const config = {
-  BUCKET_URL: process.env.BUCKET_URL || "",
-  BUCKET_NAME: process.env.BUCKET_NAME || "",
-  BUCKET_PROJECT_ID: process.env.BUCKET_PROJECT_ID || "",
-  SUPABASE_URL: process.env.SUPABASE_URL || "",
-  SUPABASE_API_KEY: process.env.SUPABASE_API_KEY || "",
-  GOOGLE_APPLICATION_CREDENTIALS:
-    process.env.GOOGLE_APPLICATION_CREDENTIALS || "",
+  BUCKET_URL: process.env.BUCKET_URL || '',
+  BUCKET_EMAIL: process.env.BUCKET_EMAIL || '',
+  BUCKET_PRIVATE_KEY: process.env.BUCKET_PRIVATE_KEY || '',
+  BUCKET_NAME: process.env.BUCKET_NAME || '',
+  BUCKET_PROJECT_ID: process.env.BUCKET_PROJECT_ID || '',
+  SUPABASE_URL: process.env.SUPABASE_URL || '',
+  SUPABASE_API_KEY: process.env.SUPABASE_API_KEY || ''
 };
 
 export default config;
