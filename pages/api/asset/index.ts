6e7e72ef9a40402bd43f5cc58935620f620073e1
Ayrton Marcazzo <ayrtonmarcazzo@gmail.com>
2021-05-07 20:22:27 -0300

    feat(files): use existing file instead of creating a new one

--- pages/api/asset/index.ts
+++ pages/api/asset/index.ts
@@ -1,8 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { add } from '../../../lib/asset';
 import formidable, { Fields, File, Files } from 'formidable';
-import { readFileSync, unlinkSync, writeFileSync } from 'fs';
-import { join } from 'path';
 
 export const config = {
   api: {
@@ -10,16 +8,8 @@ export const config = {
   }
 };
 
-const saveFile = (file: File) => {
-  const buffer = readFileSync(file.path);
-  const filePath = join(process.cwd(), 'assets', file.name!);
-
-  writeFileSync(filePath, buffer);
-  return add(filePath, file.name!).then((file) => {
-    unlinkSync(filePath);
-
-    return file;
-  });
+const uploadToBucket = (file: File) => {
+  return add(file.path, file.name!);
 };
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
@@ -28,7 +18,7 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     return form.parse(
       req,
       async function (_err: any, _fields: Fields, files: Files) {
-        const file = await saveFile(files.file as File);
+        const file = await uploadToBucket(files.file as File);
         return res.status(201).send(file);
       }
     );
